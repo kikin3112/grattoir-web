e255b5b8cae07e654d00fae7a346164090a35dbf
kikin3112 <121058216+kikin3112@users.noreply.github.com>
2024-03-22 17:03:25 -0500

    varias páginas

--- udemy_scrape.py
+++ udemy_scrape.py
@@ -1,43 +1,45 @@
 import undetected_chromedriver as uc
-from selenium import webdriver
 import pyautogui
 import time
 import os
 import win32clipboard
 
 options = uc.ChromeOptions()
+driver = uc.Chrome(use_subprocess=True, options=options)
 
-br = uc.Chrome(use_subprocess=True, options=options)  
-br.get('https://www.udemy.com/courses/search/?lang=en&lang=es&q=marketing+digital&sort=relevance&src=ukw')
-br.maximize_window()
-pyautogui.press('esc')
+def comprobante(path, archivo):
+        """
+        Comprueba si el archivo.html ya se descargó.
+        """
+        existencia = os.path.join(path, archivo + '.html')
+        return os.path.exists(existencia)
 
-time.sleep(5)
-pyautogui.hotkey('ctrl','s')
-time.sleep(1)
-pyautogui.hotkey('ctrl','c')
-time.sleep(1)
-pyautogui.press('enter')
+for i in range(1, 6):
+    url = 'https://www.udemy.com/courses/search/?lang=en&lang=es&p=' + str(i) + '&q=marketing+digital&sort=relevance&src=ukw'
+    print(url)
+    driver.get(url)
 
-win32clipboard.OpenClipboard()
-nombre = win32clipboard.GetClipboardData()
-win32clipboard.CloseClipboard()
+    pyautogui.press('esc')
 
-def check_for_file(filename, directory):
-    """
-    Check if a file with the given filename exists in the specified directory.
-    """
-    file_path = os.path.join(directory, filename + '.html')
-    return os.path.exists(file_path)
+    time.sleep(5)
+    pyautogui.hotkey('ctrl','s')
+    time.sleep(1)
+    pyautogui.press('right')
+    pyautogui.press('_')
+    pyautogui.press(str(i))
+    time.sleep(1)
+    pyautogui.hotkey('ctrl','a')
+    pyautogui.hotkey('ctrl','c')
+    time.sleep(1)
+    pyautogui.press('enter')
 
-# Specify the filename and directory to check
+    ubi = r'C:\Users\luisr\Downloads'
+    win32clipboard.OpenClipboard()
+    nombre = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
 
-filename_to_check = nombre
-download_directory = r'C:\Users\luisr\Downloads'  # Change this to your directory path
+    while not comprobante(ubi, nombre):
+        time.sleep(1)
+    time.sleep(1)
 
-while not check_for_file(filename_to_check, download_directory):
-    print(f"File '{filename_to_check}' does not exist in directory '{download_directory}'.")
-    time.sleep(2)  # Wait for 2 seconds before checking again
-
-print(f"File '{filename_to_check}' exists in directory '{download_directory}'. Stopping loop.")
-br.quit()
+driver.quit()
